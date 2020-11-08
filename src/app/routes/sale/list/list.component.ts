bdaeaa695556f08268bec0717ac7b3099bb8a62a
LorneyZizzart <yoniguty2016@gmail.com>
2020-11-08 12:37:48 -0400

    add module sale

--- src/app/routes/sale/list/list.component.ts
+++ src/app/routes/sale/list/list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MtxGridColumn } from '@ng-matero/extensions';
+import { ClientService, ConvertData } from '@shared';
 
 @Component({
   selector: 'app-sale-list',
@@ -6,12 +8,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.scss']
 })
 export class SaleListComponent implements OnInit {
+  columns: MtxGridColumn[] = COLUMNS;
+  columns_products:MtxGridColumn[] = COLUMNS_PRODUCTS;
+  loading:boolean;
+  list = [];
 
-  constructor() { 
-    console.log('entro')
+  constructor(private _clientService:ClientService) { 
   }
 
   ngOnInit() {
+    this.getClients();
+  }
+
+  log(e: any) {
+    console.log(e);
+  }
+
+  getClients(){
+    this._clientService.getClients().subscribe(data => {
+      this.list  = ConvertData.getFire(data);
+      this.loading = false;
+    })
+  }
+
+  getProducts(client){
+    this._clientService.getOrders(client.data.id).subscribe(data => {
+      for(let item of this.list){
+        if(item.id === client.data.id){
+          item.products = ConvertData.getFire(data);
+          item.cashTotal= item.products.reduce((sum, value) => (typeof value.price == "number" ? sum + value.price : sum), 0);
+        }
+      }
+      console.log(this.list);
+    })
   }
 
 }
+
+export const COLUMNS_PRODUCTS:MtxGridColumn[] = [
+  { header: 'Imagen', field: 'image', width: '80px'},
+  { header: 'Nombre', field: 'name' },
+  { header: 'Precio', field: 'price' },
+  { header: 'Categoría', field: 'categori'},
+];
+
+export const COLUMNS:MtxGridColumn[] = [
+  { header: 'Nombre completo', field: 'fullname', showExpand: true},
+  { header: 'Correo electrónico', field: 'email' }
+];
